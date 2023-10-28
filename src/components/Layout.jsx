9dde89a44e7e8447c57bd06cec87a3994e78f538
Adil Zhapar <adilzhapar2002@gmail.com>
2023-10-28 15:50:35 +0600

    upd readme

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ export const Layout = ({ children }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full h-screen overflow-hidden flex p-0 m-0">
+    <div className="w-full h-screen flex p-0 m-0">
       <div className="h-screen border-r border-[#6d6a6a] w-1/3 flex flex-col items-end">
         <div className="w-1/2 py-3 flex flex-col items-start gap-y-3">
           <img src={logo} alt="" width={46} className='bg-white' />
